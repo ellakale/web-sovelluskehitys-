f14441054fbcf3ad7fdd90f47aad7b11f7aaa2a5
ellakale <ella.kalevo@metropolia.fi>
2026-03-10 14:15:50 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,18 @@ export default defineConfig({
       input: {
         home: resolve(__dirname, 'index.html'),
         bmi: resolve(__dirname, 'bmi.html'),
+        formit: resolve(__dirname, 'formit.html'),
+        login: resolve(__dirname, 'login.html'),
+        päiväkirja: resolve(__dirname, 'paivakirja.html'),
         viikoteh: resolve(__dirname, 'viikkotehtavat.html'),
+
+        //viikkoharjoitukset
+        flexbox: resolve(__dirname, 'viikkoharjoitukset/flexbox.html'),
+        flexboxlayout: resolve(__dirname, 'viikkoharjoitukset/flexbox-layout.html'),
+        rajapinnat: resolve(__dirname, 'viikkoharjoitukset/rajapinnat.html'),
+        rajapinnatv2: resolve(__dirname, 'viikkoharjoitukset/rajapinnat-v2.html'),
+        responsiivisuus: resolve(__dirname, 'viikkoharjoitukset/responsiivisuus.html')
+
       },
     },
   },
